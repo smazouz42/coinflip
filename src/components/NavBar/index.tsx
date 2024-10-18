68f56e63fcc6299f6562346f1f6cbf14f0d3aa34
Said Mazouz <smazouz@student.1337.ma>
2024-10-18 18:53:57 +0100

    feat: adding a nav bar using some items from ChadCN UI

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+
+const NavBar = () => {
+  return (
+    <nav className="flex  items-center justify-around">
+      <div className="text-white text-2xl">
+        <h1>COINFLIP</h1>
+      </div>
+      <ul className="text-white flex gap-12 text-sm font-semibold  ">
+        <li>Buy / Sell</li>
+        <li>Grow</li>
+        <li>Markets</li>
+        <li>Business</li>
+        <li>Support</li>
+      </ul>
+      <div className="flex space-x-3">
+        <Button className="border rounded-lg h-fit p-x-2">Sign in</Button>
+        <Button className=" rounded-lg h-fit bg-gradient-to-r from-[#933FFE] to-[#18C8F0] p-x-2">Sign up</Button>
+      </div>
+    </nav>
+  );
+};
+export { NavBar };
